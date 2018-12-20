c2139b8612fd5586b620ca90b384ab0e382f8433
ERICCER <ericcer@163.com>
2018-12-20 21:35:12 +0800

    updata

--- python_repos.py
+++ python_repos.py
@@ -9,7 +9,7 @@ url = 'https://api.github.com/search/repositories?q=language:python&sort=start'
 #将这个网址储存在变量url中
 r = requests.get(url)
 #向这个url发出get请求 
-print("状态码:",r.status_code)
+print("状态码:"，r.status_code)
 
 response_dict = r.json()
 
