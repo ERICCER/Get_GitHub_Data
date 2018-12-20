e179ed247f228c76194c4daa42ce120ba33e289f
ERICCER <ericcer@163.com>
2018-12-20 20:30:17 +0800

    Update python_repos.py

--- python_repos.py
+++ python_repos.py
@@ -2,6 +2,8 @@
 #Make of python3
 #Web API
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 url = 'https://api.github.com/search/repositories?q=language:python&sort=start'
 #将这个网址储存在变量url中
@@ -11,17 +13,11 @@ print("状态码:",r.status_code)
 
 response_dict = r.json()
 
-print("总仓库数:",response_dict['total_count'])
 repo_dict = response_dict['items']
-print("存储库返回:",len(repo_dict))
 
-#研究第一个仓库
-
-print("关于最受欢迎的仓库信息：")
+names, stars = [], []
 for repo_dict in repo_dict:
-    print("\nName:",repo_dict['name'])
-    print("所有者:",repo_dict['owner']['login'])
-    print("星标：",repo_dict['stargazers_count'])
-    print("URL:",repo_dict['html_url'])
-    print("描述:",repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
 
