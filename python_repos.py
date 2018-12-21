f6ee4e2e7c3169a199cf369eaf0b57b9c86a9002
ERICCER <ericcer@163.com>
2018-12-22 07:54:34 +0800

    updata python_repos.py

--- python_repos.py
+++ python_repos.py
@@ -21,7 +21,20 @@ for repo_dict in repo_dict:
     stars.append(repo_dict['stargazers_count'])
 
 my_style = LS('#333366',base_style = LCS)
-chart = pygal.Bar(style = my_style, x_label_rotation = 45,show_legend=False)
+
+#config Begin
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.labels_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 45
+my_config.show_y_guides = False
+my_config.width = 1000
+#config Finish
+
+chart = pygal.Bar(my_config, style = my_style)
 chart.title = 'Github上热门的Python项目'
 chart.x_labels = names
 
