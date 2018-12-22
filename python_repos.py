c04ce56edd120bb8bd5dd758d0a9c7dc07425b8d
ERICCER <ericcer@163.com>
2018-12-22 10:15:54 +0800

    Update python_repos.py

--- python_repos.py
+++ python_repos.py
@@ -8,8 +8,9 @@ from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 url = 'https://api.github.com/search/repositories?q=language:java&sort=start'
 #将这个网址储存在变量url中
 r = requests.get(url)
-#向这个url发出get请求 
-print("状态码:",r.status_code)
+#向这个url发出get请求
+
+#print("状态码:",r.status_code)
 
 response_dict = r.json()
 
