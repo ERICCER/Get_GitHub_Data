ae658919cb9071a493da271bbe2ef192f2510ec3
ERICCER <ericcer@163.com>
2018-12-22 10:43:48 +0800

    Update python_repos.py

--- python_repos.py
+++ python_repos.py
@@ -5,14 +5,15 @@ import requests
 import pygal
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-url = 'https://api.github.com/search/repositories?q=language:java&sort=start'
+url = 'https://api.github.com/search/repositories?q=language:python&sort=start'
 #将这个网址储存在变量url中
 r = requests.get(url)
 #向这个url发出get请求
 
-#print("状态码:",r.status_code)
+print("状态码：",r.status_code)
 
 response_dict = r.json()
+print(response_dict)
 
 repo_dict = response_dict['items']
 
